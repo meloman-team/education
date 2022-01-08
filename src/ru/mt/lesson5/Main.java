00bd8eb1276ce64ce47c7bb1ca99d4ed28a87a27
XE <fromm2009@rambler.ru>
2022-01-08 15:44:59 +0400

    Задача 5. исходник

---
+++ src/ru/mt/lesson5/Main.java
@@ -0,0 +1,8 @@
+package ru.mt.lesson5;
+
+public class Main {
+    public static void main(String[] args) {
+        int really big number = 1455634342; // Имя переменной должно быть одним словом
+        double x = 0.75; // Переменная хранит курс (rate) японской иены (JPY)
+    }
+}
