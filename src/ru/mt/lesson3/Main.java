8ccddd2a4605b516561250e368b33e1e8094f21d
XE <fromm2009@rambler.ru>
2022-01-08 14:44:41 +0400

    Задача 3. результат

--- src/ru/mt/lesson3/Main.java
+++ src/ru/mt/lesson3/Main.java
@@ -3,6 +3,7 @@ package ru.mt.lesson3;
 public class Main {
 
     public static void main(String[] args) {
-        // System.out.println("Ничегошеньки!");
+        // исправил
+         System.out.println("Ничегошеньки!");
     }
 }
