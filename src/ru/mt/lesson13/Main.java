271e23342196584ac0fefe6a09b94f2791d7c763
XE <fromm2009@rambler.ru>
2022-01-11 19:45:28 +0400

    Задача 13. Результат

--- src/ru/mt/lesson13/Main.java
+++ src/ru/mt/lesson13/Main.java
@@ -1,14 +1,9 @@
 package ru.mt.lesson13;
 
 public class Main {
-    //Исправьте программу. Что-то не так в её структуре.
-System.out.println("Ну, я даже не знаю...");
-
     class Praktikum {
-
         public static void main(String[] args) {
-            // Весь код пишется здесь
+            System.out.println("Ну, я даже не знаю...");
         }
     }
 }
-        }
