b6650e3ac3850bc902e6272b86c073e0a53fe208
XE <fromm2009@rambler.ru>
2022-01-11 17:34:40 +0400

    Задача 9. результат

---
+++ src/ru/mt/lesson9/Main.java
@@ -0,0 +1,10 @@
+package ru.mt.lesson9;
+
+public class Main {
+    public static void main(String[] args) {
+    String hello = "Привет!";
+    String name = "Aleksey" ; // Переменная с вашим именем
+       System.out.println (hello);
+       System.out.println (name); // Здесь нужно напечатать имя
+}
+}
