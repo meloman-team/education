8597d8fa449281a2640f6726dba9d30d091e2603
XE <fromm2009@rambler.ru>
2022-01-11 18:23:05 +0400

    Задача 12.1 Исходник

---
+++ src/ru/mt/lesson12/Main1.java
@@ -0,0 +1,12 @@
+package ru.mt.lesson12;
+
+public class Main1 {
+    public static void main(String[] args){
+        // Напечатайте сообщение: «С текущим курсом вы можете купить... долларов.», подставив на место пропуска значение переменной dollars.
+        double rubles = 15000.0;
+        double rateUSD = 78.5;
+        double dollars = ...;
+
+// Напечатайте значение в долларах
+    }
+}
