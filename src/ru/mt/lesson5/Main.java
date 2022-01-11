6d8a324f56a8c25ce470c5c51228733a54ece340
Questa <Lexenia94@gmail.com>
2022-01-11 19:45:59 +0300

    Задача 5.Исходник

---
+++ src/ru/mt/lesson5/Main.java
@@ -0,0 +1,7 @@
+package ru.mt.lesson5;
+
+public class Main {
+    public static void main(String[] args) {
+    decade = 10;
+System.out.println(decade);}
+} //Опять код не работает! Исправьте ошибку.
