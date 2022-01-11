b8e10a3cf9deb1a1ca6ebca6e707f2bb8bb30af1
XE <fromm2009@rambler.ru>
2022-01-11 17:15:25 +0400

    Задача 6.2 результат

--- src/ru/mt/lesson6/Main2.java
+++ src/ru/mt/lesson6/Main2.java
@@ -2,7 +2,7 @@ package ru.mt.lesson6;
 
 public class Main2 {
     public static void main(String[] args) {
-        decade = 10;
+        int decade = 10;
         System.out.println(decade);
     }
 }
