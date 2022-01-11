df0db60f2cbf8fcf51f171c9ef07dab280de5c2c
XE <fromm2009@rambler.ru>
2022-01-11 17:36:57 +0400

    Задача 9. исходник

--- src/ru/mt/lesson9/Main.java
+++ src/ru/mt/lesson9/Main.java
@@ -2,9 +2,9 @@ package ru.mt.lesson9;
 
 public class Main {
     public static void main(String[] args) {
-    String hello = "Привет!";
-    String name = "Aleksey" ; // Переменная с вашим именем
-       System.out.println (hello);
-       System.out.println (name); // Здесь нужно напечатать имя
+        String hello = "Привет!"; //После переменной hello создайте переменную name и сохраните в ней своё имя. Напечатайте её значение после приветствия.
+... // Переменная с вашим именем
+        System.out.println(hello);
+...  // Здесь нужно напечатать имя
 }
 }
