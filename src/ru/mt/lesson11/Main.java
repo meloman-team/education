76a2b9ba4196df033e1d6566b89463f383b3fc18
XE <fromm2009@rambler.ru>
2022-01-11 18:02:28 +0400

    Задача 11. результат

--- src/ru/mt/lesson11/Main.java
+++ src/ru/mt/lesson11/Main.java
@@ -2,10 +2,9 @@ package ru.mt.lesson11;
 
 public class Main {
     public static void main(String[] args) {
-        String hello = "Привет!"; // Измените программу предыдущего задания. Пусть теперь она печатает строку вида: «Привет! Меня зовут Пиксель. Я кот». Обратите внимание, что предложения отделены пробелами.
+        String hello = "Привет!";
         String myNameIsPixel = "Меня зовут Пиксель.";
         String iAmCat = "Я кот.";
-
-        System.out.println(...); // Напечатайте фразу
+        System.out.println( hello + "" + myNameIsPixel + "" + iAmCat); //
     }
 }
