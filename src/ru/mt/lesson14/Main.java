94e5063f2cafb751275bf47e953114e45bd66f69
XE <fromm2009@rambler.ru>
2022-01-11 19:54:09 +0400

    Задача 13. Результат

---
+++ src/ru/mt/lesson14/Main.java
@@ -0,0 +1,12 @@
+package ru.mt.lesson14;
+
+public class Main {
+    class Programm {
+        //Исправьте ошибки в коде.
+        public static void main(String[] args)
+            System.out.println("Вроде несложно...")
+            System.out.println("Буду писать код дальше!");
+
+        }
+    }
+
