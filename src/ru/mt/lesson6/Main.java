792f9a41f581aa8a31ad934376fcaa555e7ac1c0
XE <fromm2009@rambler.ru>
2022-01-11 17:12:04 +0400

    Задача 6. результат

--- src/ru/mt/lesson6/Main.java
+++ src/ru/mt/lesson6/Main.java
@@ -1,8 +1,9 @@
 package ru.mt.lesson6;
 
 public class Main {
-    {...  // Объявите переменную
-...  // Присвойте переменной значение
-        System.out.println(...);
+        public static void main(String[] args){
+     double rateUSD; // Объявите переменную
+        rateUSD = 78.7;  // Присвойте переменной значение
+        System.out.println(rateUSD);
 }
 }
