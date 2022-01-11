1354f7cfc2643d874070a62d24002f39221c4aef
XE <fromm2009@rambler.ru>
2022-01-11 19:55:01 +0400

    Задача 14.Результат

--- src/ru/mt/lesson14/Main.java
+++ src/ru/mt/lesson14/Main.java
@@ -2,11 +2,10 @@ package ru.mt.lesson14;
 
 public class Main {
     class Programm {
-        //Исправьте ошибки в коде.
-        public static void main(String[] args)
-            System.out.println("Вроде несложно...")
+        public static void main(String[] args) {
+            System.out.println("Вроде несложно...");
             System.out.println("Буду писать код дальше!");
 
         }
     }
-
+}
