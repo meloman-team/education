b14e7cf763a26a0caf6b2b246e0f369fd3ebaa33
XE <fromm2009@rambler.ru>
2022-01-11 17:54:57 +0400

    Задача 10. результат

--- src/ru/mt/lesson10/Main.java
+++ src/ru/mt/lesson10/Main.java
@@ -1,10 +1,9 @@
 package ru.mt.lesson10;
 
-public class Main
-        public static void main(String[] args) {
-         //В предыдущем уроке вы объявили две переменных: hello и name. Напишите код, который будет здороваться с вами, например, «Привет!Пиксель».
+public class Main {
+    public static void main(String[] args) {
             String hello = "Привет!";
-            String name = "Пиксель"; // Замените на ваше имя
-            System.out.println(...); // Напечатайте приветствие
+            String name = "Aleksey"; // Замените на ваше имя
+            System.out.println( hello + "" + name); // Напечатайте приветствие
 }
 }
