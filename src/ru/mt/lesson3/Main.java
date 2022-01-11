9650bf25b97200d3fb9fc0f835d83f80b510c6f6
Questa <Lexenia94@gmail.com>
2022-01-11 19:17:48 +0300

    Задача 3. Решениие

--- src/ru/mt/lesson3/Main.java
+++ src/ru/mt/lesson3/Main.java
@@ -2,7 +2,7 @@ package ru.mt.lesson3;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Достоевский Михаил");
+        System.out.println("Достоевский Михаил");
         System.out.print(42);
     }
 //Доработайте код предыдущего задания так, чтобы имя и возраст печатались на разных строчках.
