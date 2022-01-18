1cd587c7697373da8b8bedf3d5a9cfd540c121ba
XE <fromm2009@rambler.ru>
2022-01-18 16:56:46 +0400

    Задача 15.Исходник

---
+++ src/ru/mt/lesson15/Main.java
@@ -0,0 +1,13 @@
+package ru.mt.lesson15;
+
+// Объявите переменные:
+//rubles — для хранения суммы сбережений в рублях;
+//rateUSD — для курса доллара;
+//dollars — для хранения суммы сбережений в долларах.
+
+public class Main {class Praktikum {
+    public static void main(String[] args) {
+        // Код пишется здесь
+    }
+}
+}
