38e8f85d81231a9e2cfe8f4b9eb9ff1d8dbc837d
XE <fromm2009@rambler.ru>
2022-01-18 18:02:55 +0400

    Задача 16.1 Исходник

---
+++ src/ru/mt/lesson16/Main1.java
@@ -0,0 +1,10 @@
+package ru.mt.lesson16;
+// Исправьте код так, чтобы печаталось true.
+class Praktikum {
+    public static void main(String[] args) {
+        String pixelCat = "Пиксель — хороший кот!";
+        boolean isGoodCat = pixelCat..."Пиксель — хороший кот!"...;
+
+        System.out.println(isGoodCat);
+    }
+}
