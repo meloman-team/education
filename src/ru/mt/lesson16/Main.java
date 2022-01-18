cae217e98b8585fbb159160d1cdb74e4be920241
XE <fromm2009@rambler.ru>
2022-01-18 18:01:04 +0400

    Задача 16. Результат

--- src/ru/mt/lesson16/Main.java
+++ src/ru/mt/lesson16/Main.java
@@ -1,13 +1,12 @@
 package ru.mt.lesson16;
-
-
-public static void main(String[] args) {
-    double rateUSD = 78.5; // Курс доллара
-    double rateEUR = 85.5; // Курс евро
-
-    // Объявите переменную и сохраните в неё результат сравнения
-        ...
-    System.out.println(...);
+// В приложении хранятся курсы доллара и евро. Ваш друг утверждает, что стоимость доллара выше или совпадает со стоимостью евро. Докажите ему, что это ложь! Сохраните результат сравнения в переменную с именем isMoreOrEqual. Напечатайте значение этой переменной.
+class Praktikum {
+    public static void main(String[] args) {
+        double rateUSD = 78.5;
+        double rateEUR = 85.5;
+        boolean isMoreOrEqual = rateUSD > rateEUR;
+        System.out.println( isMoreOrEqual );
+    }
 }
 
 
