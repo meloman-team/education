5b6ef96935416ff31395e78fc148fa8e3b7644e7
XE <fromm2009@rambler.ru>
2022-01-18 17:15:42 +0400

    Задача 15.1 Результат

--- src/ru/mt/lesson15/Main1.java
+++ src/ru/mt/lesson15/Main1.java
@@ -1,10 +1,10 @@
 package ru.mt.lesson15;
 
-//Теперь нужно сообщить пользователю о его финансовом положении. После подсчета долларов напечатайте следующую строку: «У вас на счету ... рублей. В долларах это ... долларов. Так держать!».
 class Praktikum {
     public static void main(String[] args) {
         double rubles = 30000.0;
         double rateUSD = 78.5;
         double dollars = rubles/rateUSD;
+        System.out.println("У Вас на счету" + " " + rubles + " " + "рублей." + " " + "В долларах это" + " " + dollars + " " + "долларов." + " " + "Так держать!" );
     }
 }
