f5bbca541321bc783e97a4946ba963034996ab68
XE <fromm2009@rambler.ru>
2022-01-18 17:42:37 +0400

    Задача 16. Исходник

---
+++ src/ru/mt/lesson16/Main.java
@@ -0,0 +1,13 @@
+package ru.mt.lesson16;
+
+
+public static void main(String[] args) {
+    double rateUSD = 78.5; // Курс доллара
+    double rateEUR = 85.5; // Курс евро
+
+    // Объявите переменную и сохраните в неё результат сравнения
+        ...
+    System.out.println(...);
+}
+
+
