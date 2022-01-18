7008ea2d63a401778159a159555812d294e3713f
XE <fromm2009@rambler.ru>
2022-01-18 19:19:49 +0400

    Задача 17.1 Результат

--- src/ru/mt/lesson17/Main1.java
+++ src/ru/mt/lesson17/Main1.java
@@ -1,16 +1,15 @@
 package ru.mt.lesson17;
-// Исправьте программу так, чтобы рекомендация по покупке долларов печаталась, только если курс меньше 70. Не забудьте проверить, что всё работает — поменяйте значение курса и убедитесь, что программа может дать вам совет.
+
 public class Main1 {
     public static void main(String[] args) {
-
-    double rateUSD = 78.5;
-    double rubles = 14356.5;
-
-        ...
-    System.out.println("Отличный курс доллара — рекомендую купить!");
-        ...
-
-    System.out.println("Ваши сбережения в долларах: " + rubles / rateUSD);
+        double rateUSD = 68.5;
+        double rubles = 14356.5;
+        if (rateUSD >= 70) {
+            System.out.println("Ваши сбережения в долларах: " + rubles / rateUSD);
+        } else {
+            System.out.println("Отличный курс доллара — рекомендую купить!");
+            System.out.println("Ваши сбережения в долларах: " + rubles / rateUSD);
+        }
+    }
 }
-}
-}
+
