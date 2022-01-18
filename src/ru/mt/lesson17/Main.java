8c1314e4e6cdefb109be821a64aac956d87af5cf
XE <fromm2009@rambler.ru>
2022-01-18 18:51:09 +0400

    Задача 17. Результат

--- src/ru/mt/lesson17/Main.java
+++ src/ru/mt/lesson17/Main.java
@@ -1,17 +1,16 @@
 package ru.mt.lesson17;
-// Исправьте проблему с областью видимости переменной rateUSD. Объявите её после переменной condition и там же инициализируйте — присвойте значение 78.8.
+
 public class Main  {
     public static void main(String[] args) {
 
         boolean condition = true;
-        // Перенесите объявление переменной сюда
-
-        if (condition) {
-            double rateUSD = 78.8;
-            System.out.println("Внутри блока курс доллара виден. Он равен " + rateUSD);
-        }
-        // Здесь переменная rateUSD не видна
+        double rateUSD = 78.8;
+        System.out.println("Внутри блока курс доллара виден. Он равен " + rateUSD);
+        if (condition);
         System.out.println("Вне блока курс доллара не виден. Должен быть " + rateUSD);
+        }
+
+
     }
-}
+
 
