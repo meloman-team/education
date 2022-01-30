ae4ff6265849db61e643bc43ac6834b579e3df8b
XE <fromm2009@rambler.ru>
2022-01-30 15:07:04 +0400

    Задача 18. Исходник

---
+++ src/ru/mt/lesson18/Main.java
@@ -0,0 +1,17 @@
+package ru.mt.lesson18;
+
+public class Main {
+    public static void main(String[] args) {
+
+        double moneyBeforeSalary = 15000.0;{
+
+        if (moneyBeforeSalary < 3000) {
+            System.out.println("Сегодня лучше поесть дома. Экономьте, и вы дотянете до зарплаты!");
+        } else if (moneyBeforeSalary < 10000) {
+            System.out.println("Окей, пора в Макдак!");
+        } else {
+            System.out.println("Класс! Заказывайте крабов!");
+        }
+    }
+}
+}
