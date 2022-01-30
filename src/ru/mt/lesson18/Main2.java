43c1bf8d2534dc8c9e9d34e17a87960d22c8af34
XE <fromm2009@rambler.ru>
2022-01-30 16:22:48 +0400

    Задача 18.1 Результат

--- src/ru/mt/lesson18/Main2.java
+++ src/ru/mt/lesson18/Main2.java
@@ -3,17 +3,18 @@ package ru.mt.lesson18;
 public class Main2 {
     public static void main(String[] args) {
 
-        double moneyBeforeSalary = 15000.0; // Количество денег до зарплаты
+        double moneyBeforeSalary = 30001.0;
 
         if (moneyBeforeSalary < 3000) {
             System.out.println("Сегодня лучше поесть дома. Экономьте, и вы дотянете до зарплаты!");
         } else if (moneyBeforeSalary < 10000) {
             System.out.println("Окей, пора в Макдак!");
-        } ... {
+        } else if  (moneyBeforeSalary <30000) {
             System.out.println("Неплохо! Прикупите долларов и зайдите поужинать в классное место.");
-        } ... {
+        } else {
             System.out.println("Класс! Заказывайте крабов!");
         }
     }
 }
-}
+
+
