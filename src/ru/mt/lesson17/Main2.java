fda5793603696c0b3b23b88b47ef0d56634ff667
XE <fromm2009@rambler.ru>
2022-01-30 14:47:28 +0400

    Задача 17.2 Результат

--- src/ru/mt/lesson17/Main2.java
+++ src/ru/mt/lesson17/Main2.java
@@ -1,19 +1,17 @@
 package ru.mt.lesson17;
-//Давайте ещё немного прокачаем приложение. Сделаем так, чтобы оно могло конвертировать не только в доллары, но и в евро. В переменной currency сохраните название валюты: USD или EUR. В зависимости от того, какая валюта хранится в переменной, пользователь увидит сумму своих сбережений в долларах или евро.
+
 public class Main2 {
     public static void main(String[] args) {
-    double rateUSD = 78.5;
-    double rateEUR = 85.5;
-    double rubles = 14560.5;
+        double rateUSD = 78.5;
+        double rateEUR = 85.5;
+        double rubles = 14560.5;
 
-    String currency = "USD";
-    System.out.println("Вы конвертируете рубли в " + currency);
-    // Если currency равно "USD", то конвертируем в доллары, иначе в евро
-        ... {
-        System.out.println("Ваши сбережения в долларах: " + rubles / rateUSD);
-    } ... {
-        System.out.println("Ваши сбережения в евро: " + rubles / rateEUR);
+        String currency = "EUR";
+        System.out.println("Вы конвертируете рубли в " + currency);
+        if (currency.equals("EUR")) {
+            System.out.println("Ваши сбережения в евро: " + rubles / rateEUR);
+        }  else {
+            System.out.println("Ваши сбережения в долларах: " + rubles / rateUSD);
+        }
     }
-}
-}
 }
