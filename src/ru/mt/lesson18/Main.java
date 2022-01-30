757661c1ce2d7d5ae4fa95849d3becf67ce81bad
XE <fromm2009@rambler.ru>
2022-01-30 15:09:05 +0400

    Задача 18. Результат

--- src/ru/mt/lesson18/Main.java
+++ src/ru/mt/lesson18/Main.java
@@ -3,7 +3,7 @@ package ru.mt.lesson18;
 public class Main {
     public static void main(String[] args) {
 
-        double moneyBeforeSalary = 15000.0;{
+        double moneyBeforeSalary = 5000.0;{
 
         if (moneyBeforeSalary < 3000) {
             System.out.println("Сегодня лучше поесть дома. Экономьте, и вы дотянете до зарплаты!");
